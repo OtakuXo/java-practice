e9dd6afe74707de0f9b8392707cc5b6dcf0977e4
OtakuXo <sanjaylimbu739@gmail.com>
2025-06-20 10:13:54 +0400

    minimal completed sake game

--- snake/src/main/java/com/snake/App.java
+++ snake/src/main/java/com/snake/App.java
@@ -2,13 +2,18 @@ package com.snake;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.stage.Stage;
 import javafx.scene.*;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
 public class App extends Application {
    private enum directionOptions {
@@ -32,6 +37,7 @@ public class App extends Application {
    final int snakeX[] = new int[noOfTiles];
    final int snakeY[] = new int[noOfTiles];
    private int snakeLength = 4;
+   private int score = 0;
    private gameStateOptions gameState = gameStateOptions.stopped;
 
    private int appleX;
@@ -42,6 +48,9 @@ public class App extends Application {
    Pane root = new Pane();
    final Canvas canvas = new Canvas(width, height);
    GraphicsContext gc = canvas.getGraphicsContext2D();
+   Button startBtn = new Button("Start");
+   Label scoreLabel = new Label("Score" + Integer.toString(score));
+   StackPane canvasHolder = new StackPane(canvas);
 
    public Parent createContent() {
       AnimationTimer timer = new AnimationTimer() {
@@ -49,6 +58,7 @@ public class App extends Application {
 
          @Override
          public void handle(long now) {
+            scoreLabel.setText("Score" + Integer.toString(score));
             if (gameState == gameStateOptions.running) {
                start();
             } else {
@@ -61,21 +71,39 @@ public class App extends Application {
             }
          }
       };
-      snakeX[0] = (tileSize * colTiles) / 2;
-      snakeY[0] = (tileSize * rowTiles) / 2;
-      spawnApple();
-      timer.start();
 
-      StackPane canvasHolder = new StackPane(canvas);
+      startBtn.setLayoutY(10);
+      startBtn.prefHeight(30);
+      startBtn.setOnAction(new EventHandler<ActionEvent>() {
+         @Override
+         public void handle(ActionEvent arg0) {
+            gameState = gameStateOptions.running;
+            timer.start();
+            snakeX[0] = (tileSize * colTiles) / 2;
+            snakeY[0] = (tileSize * rowTiles) / 2;
+            snakeLength = 4;
+            score = 0;
+            spawnApple();
+         }
+
+      });
+
+      scoreLabel.setLayoutY(10);
+      scoreLabel.setLayoutX(100);
+      scoreLabel.prefHeight(30);
+      scoreLabel.setFont(new Font(20.0));
+
       canvasHolder.setStyle("-fx-background-color:#000");
       canvasHolder.setLayoutY(50);
+
+      root.getChildren().add(scoreLabel);
+      root.getChildren().add(startBtn);
       root.getChildren().add(canvasHolder);
       return root;
    }
 
    public void update() {
-
-      System.out.println(gameState);
+      checkAppleEaten();
       for (int i = snakeLength; i > 0; i--) {
          snakeX[i] = snakeX[i - 1];
          snakeY[i] = snakeY[i - 1];
@@ -100,16 +128,16 @@ public class App extends Application {
 
    public void draw() {
       gc.clearRect(0, 0, width, height);
-      for (int i = 1; i < colTiles; i++) {
-         gc.setStroke(Color.WHEAT);
-         gc.setLineWidth(1);
-         gc.strokeLine(i * tileSize, 0, i * tileSize, height);
-      }
-      for (int i = 1; i < rowTiles; i++) {
-         gc.setStroke(Color.WHEAT);
-         gc.setLineWidth(1);
-         gc.strokeLine(0, i * tileSize, width, i * tileSize);
-      }
+      // for (int i = 1; i < colTiles; i++) {
+      //    gc.setStroke(Color.WHEAT);
+      //    gc.setLineWidth(1);
+      //    gc.strokeLine(i * tileSize, 0, i * tileSize, height);
+      // }
+      // for (int i = 1; i < rowTiles; i++) {
+      //    gc.setStroke(Color.WHEAT);
+      //    gc.setLineWidth(1);
+      //    gc.strokeLine(0, i * tileSize, width, i * tileSize);
+      // }
 
       for (int i = snakeLength; i >= 0; i--) {
          if (i == 0) {
@@ -121,14 +149,17 @@ public class App extends Application {
          }
       }
 
-      for (int i = 0; i < snakeLength; i++) {
-         gc.setFill(Color.RED);
-         gc.fillOval(appleX, appleY, tileSize, tileSize);
-      }
+      gc.setFill(Color.RED);
+      gc.fillOval(appleX, appleY, tileSize, tileSize);
    }
 
    public void gameOver() {
-      System.out.println(snakeY[0]);
+      for (int i = snakeLength; i > 0; i--) {
+         if (snakeX[i] == snakeX[0] && snakeY[i] == snakeY[0]) {
+            gameState = gameStateOptions.stopped;
+         }
+      }
+
       if (snakeX[0] > width - tileSize) {
          gameState = gameStateOptions.stopped;
       }
@@ -148,28 +179,51 @@ public class App extends Application {
       appleY = ((int) (Math.random() * rowTiles)) * tileSize;
    }
 
+   public void checkAppleEaten() {
+      if (snakeX[0] == appleX && snakeY[0] == appleY) {
+         snakeLength++;
+         score++;
+         spawnApple();
+      }
+   }
+
    @Override
    public void start(Stage primaryStage) throws Exception {
-      gameState = gameStateOptions.running;
       Scene scene = new Scene(createContent());
 
       scene.setOnKeyPressed(e -> {
          System.out.println(e.getCode());
          switch (e.getCode()) {
             case A:
+            case H:
             case LEFT:
+               if (direction == directionOptions.right) {
+                  return;
+               }
                direction = directionOptions.left;
                break;
             case D:
+            case L:
             case RIGHT:
+               if (direction == directionOptions.left) {
+                  return;
+               }
                direction = directionOptions.right;
                break;
             case W:
+            case K:
             case UP:
+               if (direction == directionOptions.down) {
+                  return;
+               }
                direction = directionOptions.up;
                break;
             case S:
+            case J:
             case DOWN:
+               if (direction == directionOptions.up) {
+                  return;
+               }
                direction = directionOptions.down;
                break;
             default:
